efc06ba75b5ff454340577cfad68351af5aae8fa
Juan Fco. Mange <juanfmange@gmail.com>
2023-04-08 18:09:37 -0600

    strings en java

---
+++ src/main/strings.java
@@ -0,0 +1,12 @@
+package main;
+
+public class strings {
+    public static void main(String[] args) {
+        //Strings = clase
+        String mensaje = new String("Texto");
+
+        String msg = "Hola esto es un string";
+        System.out.println(msg);
+        System.out.println(mensaje);
+    }
+}
